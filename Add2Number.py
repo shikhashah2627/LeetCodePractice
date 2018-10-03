4390c9789cea868886e2db60b97e82d10f974df2
shikhashah2627 <shikha2@pdx.edu>
2018-10-03 12:41:05 -0700

    half working code.

--- Add2Number.py
+++ Add2Number.py
@@ -5,3 +5,22 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        part_list = nums[1:]
+        for i in nums:
+            for j in part_list:
+                sum_init = i + j
+                if(sum_init == target):
+                    if (nums.index(i) == (part_list.index(j))):
+                      final_output = [nums.index(i),(part_list.index(j)+4)] 
+                    else:   
+                        final_output = [nums.index(i),(part_list.index(j)+1)]                   
+                    print (final_output)                        
+                    return final_output
+
+def main():
+    list_num = Solution()
+    list_of_num = [2,2,5,6,6,5,11]
+    list_num.twoSum(list_of_num,10)
+
+if __name__ == "__main__":
+    main()           
