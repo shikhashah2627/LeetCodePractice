bc8ac5c5dff752d4a4d6a989ed9505506ffbe410
shikhashah2627 <shikha2@pdx.edu>
2018-10-04 07:12:50 -0700

    Modified some condition.

--- Add2Number.py
+++ Add2Number.py
@@ -1,26 +1,47 @@
 class Solution(object):
+    
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        part_list = nums[1:]
-        for i in nums:
-            for j in part_list:
-                sum_init = i + j
-                if(sum_init == target):
-                    if (nums.index(i) == (part_list.index(j))):
-                      final_output = [nums.index(i),(part_list.index(j)+4)] 
-                    else:   
-                        final_output = [nums.index(i),(part_list.index(j)+1)]                   
-                    print (final_output)                        
-                    return final_output
+        d =  {index_value: list_value for index_value, list_value in enumerate(nums)}
+        print (d)
+        final = []   
+        x = 1         
+        for i in d:
+            j = i + 1
+            for j in d:
+                print i,j,i+j
+                if(d[j] == (target - d[i])):
+                    if(d[i]==d[j] and len(final) == 0):
+                        if (len(nums) == 2 and (target == (d[i]+d[j]))):
+                            final.append(i)
+                            final.append(i+1)
+                            return final
+                        else:    
+                            final.append(i)
+                            x+=1
+                            nums.remove(i)
+                            print nums
+                    #final.append(nums.index(j))
+                    elif(i==j and len(final) > 0):
+                        final.append(i+1)
+                        return final 
+                    else:
+                        final.append(i)
+                        final.append(j)
+                        return final
+        
 
+             
+        
 def main():
     list_num = Solution()
-    list_of_num = [2,2,5,6,6,5,11]
-    list_num.twoSum(list_of_num,10)
+    list_of_num = [1,3,4,2]
+    x = list_num.twoSum(list_of_num,6)
+    print (x)
 
 if __name__ == "__main__":
     main()           
